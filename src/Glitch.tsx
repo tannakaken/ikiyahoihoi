455cda061fe809edd89b70b9ffb058a1aa866b7e
Kensaku Tanaka <tannakaken@gmail.com>
2024-08-23 14:51:47 +0900

    グリッチをカスタマイズ

--- src/Glitch.tsx
+++ src/Glitch.tsx
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useMemo, useRef, useState } from 'react'
-import { VFXProvider, VFXSpan } from 'react-vfx';
+import * as ReactVFX from 'react-vfx';
 import { Novel } from './assets/novel.type';
 
 type Props = {
@@ -7,7 +7,69 @@ type Props = {
     id?: string;
 }
 
-const Char = ({char} : {char: string}) => {
+const myRgbShift = `
+    precision highp float;
+    uniform vec2 resolution;
+    uniform vec2 offset;
+    uniform float time;
+    uniform int num; // custom uniform passed as props
+    uniform sampler2D src;
+
+    float nn(float y, float t) {
+        float n = (
+            sin(y * .07 + t * 8. + sin(y * .5 + t * 10.)) +
+            sin(y * .7 + t * 2. + sin(y * .3 + t * 8.)) * .7 +
+            sin(y * 1.1 + t * 2.8) * .4
+        ) * (float(num) / 30.0) * (float(num) / 30.0);
+
+        n += sin(y * 124. + t * 100.7) * sin(y * 877. - t * 38.8) * .3;
+
+        return n;
+    }
+
+    float step2(float t, vec2 uv) {
+        return step(t, uv.x) * step(t, uv.y);
+    }
+
+    float inside(vec2 uv) {
+        return step2(0., uv) * step2(0., 1. - uv);
+    }
+
+    void main (void) {
+        vec2 uv = (gl_FragCoord.xy - offset) / resolution;
+        vec2 uvr = uv, uvg = uv, uvb = uv;
+
+        float t = mod(time, 30.);
+
+        float amp = 10. / resolution.x;
+
+        if (abs(nn(uv.y, t)) > 1.) {
+            uvr.x += nn(uv.y, t) * amp;
+            uvg.x += nn(uv.y, t + 10.) * amp;
+            uvb.x += nn(uv.y, t + 20.) * amp;
+        }
+
+        vec4 cr = texture2D(src, uvr) * inside(uvr);
+        vec4 cg = texture2D(src, uvg) * inside(uvg);
+        vec4 cb = texture2D(src, uvb) * inside(uvb);
+
+        gl_FragColor = vec4(
+            cr.r,
+            cg.g,
+            cb.b,
+            smoothstep(.0, 1., cr.a + cg.a + cb.a)
+        );
+    }
+    `;
+
+/**
+ * 大きな文字列に{@link ReactVFX.VFXSpan}や{@link ReactVFX.VFXDiv}を使うと、
+ * なぜか右側や下部が欠けたり、スマホなどの細長い画面で表示したときに、
+ * 「webgl gl_invalid_value: desired resource size is greater than max texture size.」
+ * のようなWebGLのエラーが起きたりする。
+ * いろいろ、試行錯誤をしたが、これならどうやら動くようだ。
+ */
+const Char = ({char, num} : {char: string, num: number}) => {
     const [seed, setSeed] = useState(Math.random());
     const requestIdRef = useRef<number>();
     useEffect(() => {
@@ -23,23 +85,33 @@ const Char = ({char} : {char: string}) => {
         }
     }, [])
     if (seed <= 1.0) {
-        return <VFXSpan shader={"rgbShift"}>{char}</VFXSpan>
+        return <ReactVFX.VFXSpan uniforms={{
+            num: () => num,
+        }} shader={myRgbShift}>{char}</ReactVFX.VFXSpan>
     } else {
         return <span>{char}</span>
     }
 }
 
-const VFXParagraph = ({content}: {content: string}) => {
+const VFXParagraph = ({content, num}: {content: string; num: number}) => {
     return [...content].map((char, index) => {
-        return <Char char={char} key={`char-${index}`} />
+        return <Char char={char} key={`char-${index}`} num={num} />
     })
 }
 
-const Paragraph = ({content}: {content: string}) => {
+/**
+ * あまりに大量の{@link VFXReact.VFXSpan}や{@link VFXReact.VFXDiv}を表示すると、
+ * 「omg」というエラーを出して、落ちてしまうので、
+ * 画面の外にある部分は通常のUI要素にした。
+ */
+const Paragraph = ({content, num}: {content: string; num: number}) => {
     const elementRef = useRef<HTMLParagraphElement>(null);
     const [isVisible, setIsVisible] = useState(false);
     
     useEffect(() => {
+        /**
+         * スクロールを検知して、自分が現在画面の中にいるかどうかを調べる。
+         */
         const handleScroll = () => {
             if (elementRef.current) {
                 const rect = elementRef.current.getBoundingClientRect();
@@ -62,7 +134,7 @@ const Paragraph = ({content}: {content: string}) => {
     } , []);
     return (
         <div ref={elementRef}>
-            {isVisible ? <VFXParagraph content={content} /> : <span>{content}</span>}
+            {isVisible ? <VFXParagraph content={content} num={num} /> : <span>{content}</span>}
         </div>
     )
 }
@@ -72,15 +144,18 @@ const Root = ({body, num}: {body: string, num: number}) => <div>{
         if (num < 30) {
             return <p key={`paragraph-${index}`}>{line}</p>
         }
-        return <Paragraph content={line} key={`paragrap-${index}`} />
+        return <Paragraph content={line} key={`paragrap-${index}`} num={num} />
     })}</div>
 
+    
 export const Glitch = (props: Props) => {
     const [num, setNum] = useState(0);
     const data = useMemo(() => ({
         scrollY: 0,
     }), []);
-    // リスナに登録する関数
+    /**
+     * スクロールでの戻るを検知する。
+     */
     const onScroll = useCallback(() => {
       if (window.scrollY < data.scrollY) {
         setNum(num + 1)
@@ -100,15 +175,11 @@ export const Glitch = (props: Props) => {
         document.removeEventListener('scroll', onScroll)
       }
     }, [onScroll])
+    console.warn(num);
 
     return <div id={props.id} className='container'>
-            <VFXProvider>
+            <ReactVFX.VFXProvider>
                 <Root body={props.novel.body} num={num} />
-                {/* <VFXSpan shader={"rainbow"} overflow={100} uniforms={{
-                    num: () => data.num,
-                }}>
-                    {props.novel.body}
-                </VFXSpan> */}
-            </VFXProvider>
+            </ReactVFX.VFXProvider>
         </div>
   }
